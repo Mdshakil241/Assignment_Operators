294bfd3baafd74bf37c1cddc3a43ab89d9204fc3
MD SHAKIL ISLAM <73918723+Mdshakil241@users.noreply.github.com>
2022-10-07 21:33:12 +0600

    Assignment_Operators

---
+++ main.js
@@ -0,0 +1,21 @@
+// Assignment Operators (+)
+var x = 3;
+x += 6; // x = x + 6
+document.write(x);
+
+// Assignment Operators (-)
+var x = 3;
+x -= 6; // x = x - 6
+document.write(x);
+
+// Assignment Operators (*)
+var x = 3;
+x *= 6; // x = x * 6
+document.write(x);
+
+// Assignment Operators (*)
+var x = 3;
+x /= 6; // x = x / 6
+document.write(x);
+
+
